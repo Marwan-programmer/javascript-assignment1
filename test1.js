c21e7078b9280cdc8bed7785d6c01eaf21379746
marwan <marwan.alsubayi@gmail.com>
2021-04-19 11:06:27 +0300

    clearer version

--- test1.js
+++ test1.js
@@ -3,37 +3,14 @@
    let
         step1 = prompt("value1");
     step2 = prompt("value2");
+    
 
-    step3 = step1 + step2; 
+    step3 = +step1 + +step2; 
+    step3= step3 || 0;
 
 
     alert(step3);
 
-
-
-
-
-
-
-
-
-
-//The code below deals with number and string which means if there are numbers in step1 and step2, they will be summed through the calculation
-///to use it ignore the code above by comment and use the code below alone.....
-
-
-// let
-//    step1 = prompt("value1");
-//     step2 = prompt(" value2");
-
-
-//   if((step1 >=0 || step1 <0 ) && ( step2 >=0 ||step2 <0)){
-   
-//     alert(+step1  + +step2);    //I used  Numeric conversion, unary +  https://javascript.info/operators#numeric-conversion-unary
-
-//   }else{
-//     alert(step1 + step2);
-//   }
 
 
 
