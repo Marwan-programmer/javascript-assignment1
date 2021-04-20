a7159fb70b66f4247718ae335fb3355690179b3e
marwan <marwan.alsubayi@gmail.com>
2021-04-20 10:45:26 +0300

    clearer version

--- test1.js
+++ test1.js
@@ -2,11 +2,11 @@
    
    let
         step1 = prompt("value1");
-    step2 = prompt("value2");
+    step2 = prompt("value2")||0;
     
 
-    step3 = +step1 + +step2; 
-    step3= step3 || 0;
+    step3 = (+step1||0) +(+step2||0); 
+
 
 
     alert(step3);
